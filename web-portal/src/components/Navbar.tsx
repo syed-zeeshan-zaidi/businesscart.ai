b2bb5d64786fcdbe8b6d3bc290d4c6c3ad54cbe7
Syed <syed@syed.lan>
2025-09-29 14:47:33 -0700

    Added company selector on orders history page.

--- web-portal/src/components/Navbar.tsx
+++ web-portal/src/components/Navbar.tsx
@@ -168,7 +168,7 @@ const Navbar: React.FC = () => {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
                         {userRole === 'company' && (
                           <Menu.Item>
                             {({ active }) => (
